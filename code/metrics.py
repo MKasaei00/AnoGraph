9c440a62e5dc8851d9f932dcf06bc46961dfca8f
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2024-08-19 20:46:14 +0330

    reformat python code

--- code/metrics.py
+++ code/metrics.py
@@ -2,7 +2,6 @@ import pandas as pd
 import argparse
 from sklearn import metrics
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', default='DARPA')
 parser.add_argument('--time_window', type=int, default=30)
@@ -11,62 +10,67 @@ args = parser.parse_args()
 
 
 def print_anoedge_auc_time(base_path, dataset_name, algorithm):
-	data = pd.read_csv(base_path + algorithm + "_" + dataset_name + "_score.csv", names=['score', 'label'], sep=" ")
-	time_values = pd.read_csv(base_path + algorithm + "_" + dataset_name + "_time.csv", names=['avg', 'total'], sep=" ")
+    data = pd.read_csv(base_path + algorithm + "_" + dataset_name + "_score.csv", names=['score', 'label'], sep=" ")
+    time_values = pd.read_csv(base_path + algorithm + "_" + dataset_name + "_time.csv", names=['avg', 'total'], sep=" ")
 
-	fpr, tpr, _ = metrics.roc_curve(data.label, data.score)
-	auc = metrics.roc_auc_score(data.label, data.score)
+    fpr, tpr, _ = metrics.roc_curve(data.label, data.score)
+    auc = metrics.roc_auc_score(data.label, data.score)
 
-	print("%s,%s" % (algorithm, dataset_name))
-	print("AUC: %.3f" % (auc))
-	print("Time: %s\n" % (time_values["total"].iloc[1]))
+    print("%s,%s" % (algorithm, dataset_name))
+    print("AUC: %.3f" % (auc))
+    print("Time: %s\n" % (time_values["total"].iloc[1]))
+
 
 def print_anograph_auc_time(base_path, dataset_name, time_window, edge_threshold, algorithm):
-	data = pd.read_csv(base_path + algorithm + "_" + dataset_name + "_" + str(time_window) + "_" + str(edge_threshold) + "_score.csv", names=['score', 'label'], sep=" ")
-	time_values = pd.read_csv(base_path + algorithm + "_" + dataset_name + "_" + str(time_window) + "_" + str(edge_threshold) + "_time.csv", names=['avg', 'total'], sep=" ")
+    data = pd.read_csv(
+        base_path + algorithm + "_" + dataset_name + "_" + str(time_window) + "_" + str(edge_threshold) + "_score.csv",
+        names=['score', 'label'], sep=" ")
+    time_values = pd.read_csv(
+        base_path + algorithm + "_" + dataset_name + "_" + str(time_window) + "_" + str(edge_threshold) + "_time.csv",
+        names=['avg', 'total'], sep=" ")
 
-	fpr, tpr, _ = metrics.roc_curve(data.label, data.score)
-	auc = metrics.roc_auc_score(data.label, data.score)
+    fpr, tpr, _ = metrics.roc_curve(data.label, data.score)
+    auc = metrics.roc_auc_score(data.label, data.score)
 
-	print("%s,%s" % (algorithm, dataset_name))
-	print("AUC: %.3f" % auc)
-	print("Time: %s\n" % (time_values["total"].iloc[1]))
+    print("%s,%s" % (algorithm, dataset_name))
+    print("AUC: %.3f" % auc)
+    print("Time: %s\n" % (time_values["total"].iloc[1]))
 
 
 def run_darpa():
-	print_anograph_auc_time("../results/", "DARPA", args.time_window, args.edge_threshold, "anograph")
-	print_anograph_auc_time("../results/", "DARPA", args.time_window, args.edge_threshold, "anograph_k")
-	print_anoedge_auc_time("../results/", "DARPA", "anoedge_g")
-	print_anoedge_auc_time("../results/", "DARPA", "anoedge_l")
+    print_anograph_auc_time("../results/", "DARPA", args.time_window, args.edge_threshold, "anograph")
+    print_anograph_auc_time("../results/", "DARPA", args.time_window, args.edge_threshold, "anograph_k")
+    print_anoedge_auc_time("../results/", "DARPA", "anoedge_g")
+    print_anoedge_auc_time("../results/", "DARPA", "anoedge_l")
 
 
 def run_iscx():
-	print_anograph_auc_time("../results/", "ISCX", args.time_window, args.edge_threshold, "anograph")
-	print_anograph_auc_time("../results/", "ISCX", args.time_window, args.edge_threshold, "anograph_k")
-	print_anoedge_auc_time("../results/", "ISCX", "anoedge_g")
-	print_anoedge_auc_time("../results/", "ISCX", "anoedge_l")
+    print_anograph_auc_time("../results/", "ISCX", args.time_window, args.edge_threshold, "anograph")
+    print_anograph_auc_time("../results/", "ISCX", args.time_window, args.edge_threshold, "anograph_k")
+    print_anoedge_auc_time("../results/", "ISCX", "anoedge_g")
+    print_anoedge_auc_time("../results/", "ISCX", "anoedge_l")
 
 
 def run_ids2018():
-	print_anograph_auc_time("../results/", "IDS2018", args.time_window, args.edge_threshold, "anograph")
-	print_anograph_auc_time("../results/", "IDS2018", args.time_window, args.edge_threshold, "anograph_k")
-	print_anoedge_auc_time("../results/", "IDS2018", "anoedge_g")
-	print_anoedge_auc_time("../results/", "IDS2018", "anoedge_l")
+    print_anograph_auc_time("../results/", "IDS2018", args.time_window, args.edge_threshold, "anograph")
+    print_anograph_auc_time("../results/", "IDS2018", args.time_window, args.edge_threshold, "anograph_k")
+    print_anoedge_auc_time("../results/", "IDS2018", "anoedge_g")
+    print_anoedge_auc_time("../results/", "IDS2018", "anoedge_l")
 
 
 def run_ddos2019():
-	print_anograph_auc_time("../results/", "DDOS2019", args.time_window, args.edge_threshold, "anograph")
-	print_anograph_auc_time("../results/", "DDOS2019", args.time_window, args.edge_threshold, "anograph_k")
-	print_anoedge_auc_time("../results/", "DDOS2019", "anoedge_g")
-	print_anoedge_auc_time("../results/", "DDOS2019", "anoedge_l")
+    print_anograph_auc_time("../results/", "DDOS2019", args.time_window, args.edge_threshold, "anograph")
+    print_anograph_auc_time("../results/", "DDOS2019", args.time_window, args.edge_threshold, "anograph_k")
+    print_anoedge_auc_time("../results/", "DDOS2019", "anoedge_g")
+    print_anoedge_auc_time("../results/", "DDOS2019", "anoedge_l")
 
 
 if __name__ == "__main__":
-	if args.dataset == 'DARPA':
-		run_darpa()
-	elif args.dataset == 'ISCX':
-		run_iscx()
-	elif args.dataset == 'IDS2018':
-		run_ids2018()
-	elif args.dataset == 'DDOS2019':
-		run_ddos2019()
+    if args.dataset == 'DARPA':
+        run_darpa()
+    elif args.dataset == 'ISCX':
+        run_iscx()
+    elif args.dataset == 'IDS2018':
+        run_ids2018()
+    elif args.dataset == 'DDOS2019':
+        run_ddos2019()
